2e1b2ef4e065e35f10aaa4e48544bdd1800f7293
Lee Sheng <lsheng@wibidata.com>
2013-02-05 23:51:56 -0800

    KIJIMRLIB-2. Add CSVBulkImporter that provides the ability to bulk import both CSV and TSV files

---
+++ kiji-mapreduce-lib/src/main/java/org/kiji/mapreduce/lib/bulkimport/CSVBulkImporter.java
@@ -0,0 +1,172 @@
+/**
+ * (c) Copyright 2013 WibiData, Inc.
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kiji.mapreduce.lib.bulkimport;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.io.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.kiji.annotations.ApiAudience;
+import org.kiji.annotations.Inheritance;
+import org.kiji.hadoop.configurator.HadoopConf;
+import org.kiji.mapreduce.KijiTableContext;
+import org.kiji.mapreduce.lib.util.CSVParser;
+import org.kiji.schema.EntityId;
+import org.kiji.schema.KijiColumnName;
+
+/**
+ * Bulk importer that handles comma separated files.  TSVs are also supported by setting the
+ * CONF_FIELD_DELIMITER configuration object.
+ * {@inheritDoc}
+ */
+@ApiAudience.Public
+@Inheritance.Sealed
+public final class CSVBulkImporter extends DescribedInputTextBulkImporter {
+  private static final Logger LOG =
+      LoggerFactory.getLogger(CSVBulkImporter.class);
+
+  /** Configuration variable that specifies the header row to use instead of the first line. */
+  public static final String CONF_INPUT_HEADER_ROW = "kiji.import.text.column.header_row";
+
+  /** Configuration variable that specifies the cell value separator in the text input files. */
+  public static final String CONF_FIELD_DELIMITER = "kiji.import.text.field.separator";
+
+  private static final String CSV_DELIMITER = ",";
+  private static final String TSV_DELIMITER = "\t";
+
+  /** The string that separates the columns of data in the input file. */
+  @HadoopConf(key=CONF_FIELD_DELIMITER)
+  private String mColumnDelimiter = CSV_DELIMITER;
+
+  private Map<String, Integer> mFieldMap = null;
+
+  /** {@inheritDoc} */
+  @Override
+  public void setup(KijiTableContext context) throws IOException {
+    super.setup(context);
+
+    // If the header row is specified in the configuration, use that.
+    if (getConf().get(CONF_INPUT_HEADER_ROW) != null) {
+      List<String> fields = null;
+      String confInputHeaderRow = getConf().get(CONF_INPUT_HEADER_ROW);
+      try {
+        fields = split(confInputHeaderRow);
+      } catch (ParseException pe) {
+        LOG.error("Unable to parse header row: {} with exception {}",
+            confInputHeaderRow, pe.getMessage());
+        throw new IOException("Unable to parse header row: " + confInputHeaderRow);
+      }
+      initializeHeader(fields);
+    }
+  }
+
+  /**
+   * Initializes the field to column position mapping for this file.
+   * @param headerFields the header fields for this delimited file.
+   */
+  private void initializeHeader(List<String> headerFields) {
+    LOG.info("Initializing field map with fields: " + StringUtils.join(headerFields, ","));
+    Map<String, Integer> fieldMap = Maps.newHashMap();
+    for (int index=0; index < headerFields.size(); index++) {
+      fieldMap.put(headerFields.get(index), index);
+    }
+    mFieldMap = ImmutableMap.copyOf(fieldMap);
+  }
+
+  /**
+   * Wrapper around CSV or TSV parsers based on the configuration of this job builder.
+   * @return a list of fields split by the mColumnDelimiter.
+   * @param line the line to split
+   * @throws ParseException if the parser encounters an error while parsing
+   */
+  private List<String> split(String line) throws ParseException {
+    if (CSV_DELIMITER.equals(mColumnDelimiter)) {
+      return CSVParser.parseCSV(line);
+    } else if (TSV_DELIMITER.equals(mColumnDelimiter)) {
+      return CSVParser.parseTSV(line);
+    }
+    throw new ParseException("Unrecognized delimiter: " + mColumnDelimiter, 0);
+  }
+
+  /**
+   * Generates the entity id for this imported line.
+   * Called within the produce() method.
+   * This implementation returns the first entry in <code>entries</code> as the EntityId.
+   * Override this method to specify a different EntityId during the produce() method.
+   *
+   * @param fields One line of input text split on the column delimiter.
+   * @param context The context used by the produce() method.
+   * @return The EntityId for the data that gets imported by this line.
+   */
+  protected EntityId getEntityId(List<String> fields, KijiTableContext context) {
+    //TODO Extend this to support composite row key ids
+    String rowkey = fields.get(mFieldMap.get(getEntityIdSource()));
+    return context.getEntityId(rowkey);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void produce(Text value, KijiTableContext context) throws IOException {
+    // This is the header line since fieldList isn't populated
+    if (mFieldMap == null) {
+      List<String> fields = null;
+      try {
+        fields = split(value.toString());
+      } catch (ParseException pe) {
+        LOG.error("Unable to parse header row: {} with exception {}",
+            value.toString(), pe.getMessage());
+        throw new IOException("Unable to parse header row: " + value.toString());
+      }
+      initializeHeader(fields);
+      // Don't actually import this line
+      return;
+    }
+
+    List<String> fields = null;
+    try {
+      fields = split(value.toString());
+    } catch (ParseException pe) {
+      LOG.error("Unable to parse line: {} with exception {}",
+          value.toString(), pe.getMessage());
+      //TODO Emit this to a rejected output so that import can be reattempted
+      return;
+    }
+    for (KijiColumnName kijiColumnName : getColumns()) {
+      final EntityId eid = getEntityId(fields, context);
+      String source = getSource(kijiColumnName);
+
+      if (mFieldMap.get(source) < fields.size()) {
+        String fieldValue = fields.get(mFieldMap.get(source));
+        context.put(eid, kijiColumnName.getFamily(), kijiColumnName.getQualifier(), fieldValue);
+      } else {
+        LOG.warn("Detected trailing empty field: " + source);
+      }
+    }
+
+  }
+}
---
+++ kiji-mapreduce-lib/src/main/java/org/kiji/mapreduce/lib/bulkimport/DescribedInputTextBulkImporter.java
@@ -0,0 +1,190 @@
+/**
+ * (c) Copyright 2012 WibiData, Inc.
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kiji.mapreduce.lib.bulkimport;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import com.google.common.base.Preconditions;
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.kiji.annotations.ApiAudience;
+import org.kiji.annotations.Inheritance;
+import org.kiji.hadoop.configurator.HadoopConf;
+import org.kiji.hadoop.configurator.HadoopConfigurator;
+import org.kiji.mapreduce.KijiBulkImporter;
+import org.kiji.mapreduce.KijiConfKeys;
+import org.kiji.mapreduce.KijiTableContext;
+import org.kiji.mapreduce.bulkimport.KijiTableImportDescriptor;
+import org.kiji.schema.Kiji;
+import org.kiji.schema.KijiColumnName;
+import org.kiji.schema.KijiTable;
+import org.kiji.schema.KijiURI;
+import org.kiji.schema.KijiURIException;
+import org.kiji.schema.layout.KijiTableLayout;
+import org.kiji.schema.util.ResourceUtils;
+
+/**
+ * Importing from a text file requires specifying a KijiColumnName, and the source
+ * for each element to be inserted into kiji, in addition to the raw import data.
+ *
+ * <p>Use this Mapper over text files to import data into a Kiji
+ * table.  Each line in the file will be treated data for one row.
+ * This line should generate a single EntityId to write to, and any number
+ * of writes to add to that entity.  Override the produce(String, Context)
+ * method with this behavior.</p>
+ *
+ * <p>This class reads a list of qualifiers and writer Schemas from an input-descriptor file
+ * in hdfs.  See {@link org.kiji.mapreduce.bulkimport.KijiTableImportDescriptor} for
+ * input-descriptor format.</p>
+ *
+ * <p>The KijiColumnNames may be retrieved with the method getColumns().  The paired source
+ * can be retrieved with subsequent calls to getSource.</p>
+ */
+@ApiAudience.Public
+@Inheritance.Extensible
+public abstract class DescribedInputTextBulkImporter extends KijiBulkImporter<LongWritable, Text> {
+  private static final Logger LOG = LoggerFactory.getLogger(DescribedInputTextBulkImporter.class);
+
+  /**
+   * Location of writer layout file.  File names columns and schemas, and implies
+   * ordering of columns in delimited read-in file.
+   */
+  public static final String CONF_FILE = "kiji.import.text.input.descriptor.path";
+
+  /** Table layout of the output table. */
+  private KijiTableLayout mOutputTableLayout;
+
+  /** Table import descriptor for this bulk load. */
+  private KijiTableImportDescriptor mTableImportDescriptor;
+
+  /** {@inheritDoc} */
+  @Override
+  public void setConf(Configuration conf) {
+    super.setConf(conf);
+    HadoopConfigurator.configure(this);
+
+    KijiURI uri;
+    try {
+      uri = KijiURI.newBuilder(conf.get(KijiConfKeys.KIJI_OUTPUT_TABLE_URI)).build();
+    } catch (KijiURIException kue) {
+      throw new RuntimeException(kue);
+    }
+
+    Kiji kiji = null;
+    try {
+      kiji = Kiji.Factory.open(uri, conf);
+
+      KijiTable table = null;
+      try {
+        table = kiji.openTable(uri.getTable());
+        mOutputTableLayout = table.getLayout();
+      } finally {
+        IOUtils.closeQuietly(table);
+      }
+    } catch (IOException ex) {
+      throw new RuntimeException(ex);
+    } finally {
+      ResourceUtils.releaseOrLog(kiji);
+    }
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void setup(KijiTableContext context) throws IOException {
+    Preconditions.checkNotNull(mOutputTableLayout);
+    mTableImportDescriptor.validateDestination(mOutputTableLayout);
+  }
+
+  /**
+   * Converts a line of text to a set of writes to <code>context</code>, and
+   * an EntityId for the row.
+   *
+   * @param line The line to parse.
+   * @param context The context to write to.
+   * @throws IOException if there is an error.
+   */
+  public abstract void produce(Text line, KijiTableContext context)
+      throws IOException;
+
+  /** {@inheritDoc} */
+  @Override
+  public void produce(LongWritable fileOffset, Text line, KijiTableContext context)
+      throws IOException {
+    produce(line, context);
+  }
+
+  /** @return a collection of the columns for this bulk importer. */
+  protected Collection<KijiColumnName> getColumns() {
+    return mTableImportDescriptor.getColumnNameSourceMap().keySet();
+  }
+
+  /**
+   * Returns the source for the specified column.
+   * @param kijiColumnName the requested Kiji column
+   * @return the source for the requested column
+   */
+  protected String getSource(KijiColumnName kijiColumnName) {
+    return mTableImportDescriptor.getColumnNameSourceMap().get(kijiColumnName);
+  }
+
+  /** @return the source for the EntityId. */
+  protected String getEntityIdSource() {
+    return mTableImportDescriptor.getEntityIdSource();
+  }
+
+  /**
+   * Sets the input descriptor.
+   *
+   * @param inputDescriptorFile The input descriptor path.
+   */
+  @HadoopConf(key=CONF_FILE, usage="The input descriptor file.")
+  protected void setInputDescriptorPath(String inputDescriptorFile) {
+
+    if (null == inputDescriptorFile || inputDescriptorFile.isEmpty()) {
+      // Remind the user to specify this path.
+      LOG.error("No input-descriptor path specified.");
+      throw new RuntimeException("No input descriptor file specified on the Configuration."
+          + "  Did you specify the " + CONF_FILE + " variable?");
+    }
+
+    Path descriptorPath = new Path(inputDescriptorFile);
+    try {
+      LOG.info("Parsing input-descriptor file: " + descriptorPath.toString());
+      FileSystem fs = descriptorPath.getFileSystem(getConf());
+      FSDataInputStream inputStream = fs.open(descriptorPath);
+      mTableImportDescriptor =
+          KijiTableImportDescriptor.createFromEffectiveJson(inputStream);
+
+    } catch (IOException ioe) {
+      LOG.error("Could not read input-descriptor file: " + descriptorPath.toString());
+      throw new RuntimeException("Could not read file: " + descriptorPath.toString());
+    }
+  }
+}
+
