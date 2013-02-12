5c77285faa972c027e0d17a79a9ff08f0a5557dc
Lee Sheng <lsheng@wibidata.com>
2013-02-12 13:48:16 -0800

    KIJIMR-72. Adds the KijiTableImportDescriptor from Kiji MapReduce

--- kiji-mapreduce-lib/src/main/java/org/kiji/mapreduce/lib/bulkimport/CommonLogBulkImporter.java
+++ kiji-mapreduce-lib/src/main/java/org/kiji/mapreduce/lib/bulkimport/CommonLogBulkImporter.java
@@ -38,7 +38,7 @@ import org.kiji.schema.KijiColumnName;
  * Bulk importer that handles the NCSA Common Log Format.
  *
  * Details about this format here:
- * {@linkurl http://www.w3.org/Daemon/User/Config/Logging.html#common-logfile-format}
+ * {@linktourl http://www.w3.org/Daemon/User/Config/Logging.html#common-logfile-format}
  *
  * {@inheritDoc}
  */
